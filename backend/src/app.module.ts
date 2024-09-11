b5b57bddca7171388db98931fca7ff99e3247a27
Federico Garcia <federicoignaciogarcia@gmail.com>
2024-09-11 11:52:22 -0300

    Se agregó la configuración base de la BD y composición de Pokemones

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonesController } from './pokemones/pokemones.controller';
+import { PokemonesModule } from './pokemones/pokemones.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    PokemonesModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: '../../pokemonesdb.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,           
+    }),
+  ],
+  controllers: [AppController, PokemonesController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ backend/src/pokemones/pokemones.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PokemonesController } from './pokemones.controller';
+import { PokemonesService } from './pokemones.service';
+
+@Module({
+  controllers: [PokemonesController],
+  providers: [PokemonesService]
+})
+export class PokemonesModule {}
