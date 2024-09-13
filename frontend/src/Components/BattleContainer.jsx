c6b27262ec531a8881789dede5941f119e51cb8e
Federico <federicoignaciogarcia@gmail.com>
2024-09-12 23:29:56 -0300

    Agregado de mensaje de ganador en interfaz de Front

--- frontend/src/Components/BattleContainer.jsx
+++ frontend/src/Components/BattleContainer.jsx
@@ -1,4 +1,4 @@
-import { Button, CardMedia, CardContent, Card, LinearProgress } from '@mui/material';
+import { Button, CardMedia, CardContent, Card, LinearProgress, Paper, Typography } from '@mui/material';
 // import { Axios } from 'axios';
 import Axios from "axios";
 import React, { useEffect, useState } from 'react'
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react'
 const BattleContainer = ({selectedPokemon, pokemones}) => {
 
     const [enemyPokemon,setEnemyPokemon] = useState(pokemones[2])
+    const [ganador,setGanador] = useState("")
 
     const setRandomEnemy = ()=>{
         const randomEnemy = Math.floor(Math.random() * pokemones.length);
@@ -26,6 +27,7 @@ const BattleContainer = ({selectedPokemon, pokemones}) => {
         }).then((result)=>{
             console.log("Éxito: Se enviaron los pokemones ",selectedPokemon," y ",newEnemy," con éxito")
             console.log("Resultado de la batalla:",result)
+            setGanador(result.data)
         }).catch((e)=>{
             console.log("Error: No se pudo enviar los pokemones")
             console.log(e)
@@ -39,47 +41,54 @@ const BattleContainer = ({selectedPokemon, pokemones}) => {
     },[pokemones])
 
     return (
-        <div className="battleContainer">
-            <Card className='battlePokemon selectedPokemon'> 
-                <CardMedia
-                    component="img"
-                    alt={selectedPokemon.name}
-                    image={selectedPokemon.imageUrl}
-                />
-                <CardContent>
-                    <h2>{selectedPokemon.name}</h2>
-                    <p>HP</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.hp*10} />
-                    <p>ATTACK</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.attack*10} />
-                    <p>DEFENSE</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.defense*10} />
-                    <p>SPEED</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.speed*10} />
-                </CardContent>
-            </Card>
-            <Button 
-                onClick={()=>{startBattle()}}
-                variant="contained"
-                color="success"
-                style={{height: 'fit-content'}}>
-                Start Battle
-            </Button>
-            {enemyPokemon && (
-                <>
-                    <Card className='battlePokemon enemyPokemon'> 
-                        <CardMedia
-                            component="img"
-                            alt={enemyPokemon.name}
-                            image={enemyPokemon.imageUrl}
-                        />
-                        <CardContent>
-                            <h2>{enemyPokemon.name}</h2>
-                            <p>HP</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.hp*10} />
-                            <p>ATTACK</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.attack*10} />
-                            <p>DEFENSE</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.defense*10} />
-                            <p>SPEED</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.speed*10} />
-                        </CardContent>
-                    </Card>
-                </>
+        <>
+            {ganador && (
+                <div className='battleResultContainer'>
+                    <p>{ganador}</p>
+                </div>
             )}
-        </div>
+            <div className="battleContainer">
+                <Card className='battlePokemon selectedPokemon'> 
+                    <CardMedia
+                        component="img"
+                        alt={selectedPokemon.name}
+                        image={selectedPokemon.imageUrl}
+                        />
+                    <CardContent>
+                        <h2>{selectedPokemon.name}</h2>
+                        <p>HP</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.hp*10} />
+                        <p>ATTACK</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.attack*10} />
+                        <p>DEFENSE</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.defense*10} />
+                        <p>SPEED</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={selectedPokemon.speed*10} />
+                    </CardContent>
+                </Card>
+                <Button 
+                    onClick={()=>{startBattle()}}
+                    variant="contained"
+                    color="success"
+                    style={{height: 'fit-content'}}>
+                    Start Battle
+                </Button>
+                {enemyPokemon && (
+                    <>
+                        <Card className='battlePokemon enemyPokemon'> 
+                            <CardMedia
+                                component="img"
+                                alt={enemyPokemon.name}
+                                image={enemyPokemon.imageUrl}
+                                />
+                            <CardContent>
+                                <h2>{enemyPokemon.name}</h2>
+                                <p>HP</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.hp*10} />
+                                <p>ATTACK</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.attack*10} />
+                                <p>DEFENSE</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.defense*10} />
+                                <p>SPEED</p><LinearProgress sx={{ height: 10, borderRadius: 10 }} variant="determinate" value={enemyPokemon.speed*10} />
+                            </CardContent>
+                        </Card>
+                    </>
+                )}
+            </div>
+        </>
     )
 }
 
